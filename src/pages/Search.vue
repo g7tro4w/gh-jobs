d3bf78fb874a1b152f6b634e5b32a9f23ea710f0
g7tro4w <g7tro4w@gmail.com>
2019-06-27 19:50:57 +0300

    Feat(Search): Added search page, changed router, removed default files

---
+++ src/pages/Search.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="search-page">
+        <header>
+            <h1>Greatest header of ALL!!!</h1>
+        </header>
+        <main>
+            <div class="search-form">
+                <h2>Search form is here!</h2>
+            </div>
+            <div class="search-results">
+                <h2>And here we have a results of serching!</h2>
+            </div>
+        </main>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
