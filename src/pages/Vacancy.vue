9a435c81f9d0b07387562840feb2bb1fc437f0ea
aegorov <ae@b2basket.ru>
2019-07-01 11:53:22 +0300

    Feat(Vacancy): Added spinner to vacancy page

--- src/pages/Vacancy.vue
+++ src/pages/Vacancy.vue
@@ -1,20 +1,34 @@
 <template>
-  <div>Hello from vacancy page! {{ vacancy.title }}</div>
+
+  <div>
+    <spinner v-if="attemptLoading"/>
+    <div v-else>
+      Hello from vacancy page! {{ vacancy.title }}
+    </div>
+  </div>
 </template>
 <script>
 
 import API from '@/api/index.js'
 
+import Spinner from '@/components/Spinner'
+
 export default {
+  components: {
+    'spinner': Spinner
+  },
   data () {
     return {
-      vacancy: {}
+      vacancy: {},
+      attemptLoading: false
     }
   },
   created () {
+    this.attemptLoading = true
     API.positions.read(this.$route.params.id)
       .then(positions => {
         this.vacancy = positions
+        this.attemptLoading = false
       })
   }
 }
