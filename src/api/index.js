d86f5a8175e42b77137b9cab3ac38d2702e7ea58
aegorov <ae@b2basket.ru>
2019-07-01 12:15:49 +0300

    Docs(API): Small fixed of api docs

--- src/api/index.js
+++ src/api/index.js
@@ -9,9 +9,9 @@ export default {
     * @param {Object} positionParams - search params object
     * @typedef {Object} positionParams
     * @property {String} description - A search term. Inputs in search field.
-    * @property {String} location -  A city name, zip code, or other location search term.
-    * @property {String} [lat] - A specific latitude.
-    * @property {String} [long] - A specific longitude.
+    * @property {String} [location] -  A city name, zip code, or other location search term.
+    * @property {String} [lat] - A specific latitude. If you use this, don't send location.
+    * @property {String} [long] - A specific longitude. If you use this, don't send location.
     * @property {Boolean} [full_time] - Is limit results to full time positions
     * @return {Promise} - Promise object with data or error
     */
@@ -31,7 +31,7 @@ export default {
       })
     },
     /**
-    * Gets list of jobs by parameters of search
+    * Gets information about single job position.
     *
     * @param {String} id - id of single job position
     * @param {Boolean} markdown - Set to 'true' to get the description and how_to_apply fields as Markdown
