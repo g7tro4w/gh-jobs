4948d9c9d60e9670a5b94d0607c0000d32de1e87
aegorov <ae@b2basket.ru>
2019-07-01 13:24:14 +0300

    Fix(App): Fixed main layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
     <header>
-            <h1>GitHub Jobs</h1>
-            <h2>GitHub Jobs</h2>
-        </header>
-        <main>
-          <router-view/>
-          </main>
+      <h1>GitHub Jobs</h1>
+      <h2>Find your dream job here!</h2>
+    </header>
+    <main>
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <style>
+  #app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  header {
+    width: 100%;
+    height: 100px;
+    flex: 0 0;
+    text-align: center;
+  }
+  main {
+    width: 80%;
+    flex: 1 0;
+  }
 </style>
